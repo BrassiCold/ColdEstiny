fa842299ea824410fdec16a54eb83bbaa6576314
BrassiCold <brassicold@outlook.com>
2023-05-24 20:05:09 +0800

    [1.0.0] Update

--- src/main/kotlin/fun/brassicold/plugin/coldestiny/ColdEstiny.kt
+++ src/main/kotlin/fun/brassicold/plugin/coldestiny/ColdEstiny.kt
@@ -1,16 +1,29 @@
 package `fun`.brassicold.plugin.coldestiny
 
+import `fun`.brassicold.plugin.coldestiny.internal.ColdEstinyLoader
 import taboolib.common.platform.Plugin
 import taboolib.common.platform.function.info
+import taboolib.module.configuration.Config
+import taboolib.module.configuration.ConfigFile
 
 object ColdEstiny : Plugin() {
 
+    @Config("setting.yml", migrate = true)
+    lateinit var setting: ConfigFile
+
     override fun onLoad() {
-        info("Loading")
+        ColdEstinyLoader.load()
     }
 
     override fun onEnable() {
-        info("Successfully running ExamplePlugin!")
+        ColdEstinyLoader.enable()
     }
 
+    override fun onDisable() {
+        ColdEstinyLoader.disable()
+    }
+
+    override fun onActive() {
+        ColdEstinyLoader.active()
+    }
 }
---
+++ src/main/kotlin/fun/brassicold/plugin/coldestiny/internal/ColdEstinyLoader.kt
@@ -0,0 +1,19 @@
+package `fun`.brassicold.plugin.coldestiny.internal
+
+object ColdEstinyLoader {
+    fun load() {
+
+    }
+
+    fun enable() {
+
+    }
+
+    fun disable() {
+
+    }
+
+    fun active() {
+
+    }
+}
